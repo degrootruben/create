b52ce571cef37af00f7f8cb8b8a5a3ca3c698421
Ruben de Groot <rpg.degroot@gmail.com>
2021-05-04 23:02:50 +0200

    Added some extra code for argument parsing

--- main.cpp
+++ main.cpp
@@ -1,24 +1,51 @@
 #include <iostream>
+#include <filesystem>
 
 using namespace std;
+namespace fs = std::filesystem;
+
+int mode = 0;
+int project = 0;
+const int SCHOOL = 1,
+          CODE = 2,
+          MUSIC = 3,
+          PROJECT = 4,
+          HELP = 5;
 
 int main(int argc, char *argv[]) {
     if (argc > 1) {
         string arg1 = argv[1];
-        
-        if (arg1 == "school" || arg1 == "s") {        // school    
 
+        if (arg1 == "school" || arg1 == "s") {        // school
+            mode = PROJECT;
+            project = SCHOOL;
         } else if (arg1 == "code" || arg1 == "c") {   // code
-
+            mode = PROJECT;
+            project = CODE;
         } else if (arg1 == "music" || arg1 == "m") {  // music
-
-        } else if (arg1 == "-h" || arg1 == "h" 
-                    || arg1 == "help") {              // help
-            cout << "Help menu" << endl;
+            mode = PROJECT;
+            project = MUSIC;
+        } else if (arg1 == "-h" || arg1 == "h" || arg1 == "help") {              // help
+            mode = HELP;
         }
     } else {
         cout << "No arguments found. Use -h for help." << endl;
     }
 
-    return 1;
+    if (mode == PROJECT) {
+        if (argc > 2) {
+            string projectName = argv[2];
+
+            fs::create_directory(projectName);
+        } else {
+            cout << "Please provide a project name!" << endl;
+        }
+    } else if (mode == HELP) {
+        cout << "Help menu" << endl;
+    } else if (mode == 0) {
+        cout << "Error: mode is 0." << endl;
+        return 1;
+    }
+
+    return 0;
 }
